9b6e1a445084756860cbed195afa0c23df30c991
Indrek Ruubel <indrekruubel@gmail.com>
2019-02-25 00:40:12 +0200

    dev

--- src/main/java/com/ruubel/massfollow/Application.java
+++ src/main/java/com/ruubel/massfollow/Application.java
@@ -20,7 +20,7 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... strings) {
-//        followJob.execute();
+        followJob.execute();
     }
 
 }
