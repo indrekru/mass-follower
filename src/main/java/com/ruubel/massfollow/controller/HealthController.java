6c6a747cda8ad4cdb7f13d3c7577eee43f51c30a
Indrek Ruubel <indrekruubel@gmail.com>
2019-02-28 23:16:39 +0200

    followed endpoint

--- src/main/java/com/ruubel/massfollow/controller/HealthController.java
+++ src/main/java/com/ruubel/massfollow/controller/HealthController.java
@@ -1,5 +1,8 @@
 package com.ruubel.massfollow.controller;
 
+import com.ruubel.massfollow.model.Followed;
+import com.ruubel.massfollow.service.FollowPersistenceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +10,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/health")
+@RequestMapping("/api/v1")
 public class HealthController {
 
-    @GetMapping
+    private FollowPersistenceService followPersistenceService;
+
+    @Autowired
+    public HealthController(FollowPersistenceService followPersistenceService) {
+        this.followPersistenceService = followPersistenceService;
+    }
+
+    @GetMapping("/health")
     public ResponseEntity<Map<String, Object>> health() {
         return new ResponseEntity(new HashMap<String, Object>(){{
             put("alive", true);
         }}, HttpStatus.OK);
+    }
+
+    @GetMapping("/followed")
+    public ResponseEntity followed() {
+        List<Followed> followed = followPersistenceService.findAll();
+        return new ResponseEntity<>(followed, HttpStatus.OK);
     }
 }
