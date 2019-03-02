2dfd7e2fb5890ab3e5da7a72dcebe1ec7a15e0a7
Indrek Ruubel <indrekruubel@gmail.com>
2019-03-02 15:35:48 +0200

    cors fix

--- src/main/java/com/ruubel/massfollow/config/SimpleCORSFilter.java
+++ src/main/java/com/ruubel/massfollow/config/SimpleCORSFilter.java
@@ -19,7 +19,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
