ffb6a13729f8531720e830bba68972118f44721c
Indrek Ruubel <indrekruubel@gmail.com>
2019-03-06 08:52:44 +0200

    added test

--- src/test/groovy/com/ruubel/massfollow/job/FollowJobSpec.groovy
+++ src/test/groovy/com/ruubel/massfollow/job/FollowJobSpec.groovy
@@ -22,21 +22,31 @@ class FollowJobSpec extends Specification {
         job = new FollowJob(followService, unfollowService, followingAmountService, taskExecutor)
     }
 
+    def "when execute is run and followers are done first, then after updates followers in DB" () {
+        when:
+            job.execute()
+        then:
+            1 * followService.getImFollowingAndMyFollowers() >> [3499, 2000]
+            1 * followService.execute(_)
+            1 * unfollowService.execute()
+            1 * followService.getImFollowingAndMyFollowers() >> [3666, 2666]
+            1 * followingAmountService.saveFollowingAmounts(3666, 2666)
+    }
+
     def "when following less than 3500, then runs follow and unfollow once" () {
         when:
             job.doLogic(0)
         then:
-            1 * followService.getImFollowingAndMyFollowers() >> 3499
+            1 * followService.getImFollowingAndMyFollowers() >> [3499, 2000]
             1 * followService.execute(_)
             1 * unfollowService.execute()
-
     }
 
     def "when following returns more than 3500 twice, then gives up" () {
         when:
             job.doLogic(0)
         then:
-            2 * followService.getImFollowingAndMyFollowers() >> 3500
+            2 * followService.getImFollowingAndMyFollowers() >> [3500, 2000]
             1 * unfollowService.execute()
             0 * followService.execute(_)
     }
@@ -45,8 +55,8 @@ class FollowJobSpec extends Specification {
         when:
             job.doLogic(0)
         then:
-            1 * followService.getImFollowingAndMyFollowers() >> 3500
-            1 * followService.getImFollowingAndMyFollowers() >> 3000
+            1 * followService.getImFollowingAndMyFollowers() >> [3500, 2000]
+            1 * followService.getImFollowingAndMyFollowers() >> [3000, 2000]
             1 * unfollowService.execute()
             1 * followService.execute(_)
     }
