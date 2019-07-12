a2b02640e1a1e5c66b8548695e9d0ae6a345fd56
Indrek Ruubel <indrekruubel@gmail.com>
2019-07-12 18:27:10 +0300

    cleanup job tests

--- src/test/groovy/com/ruubel/massfollow/job/FollowStatsCleanupJobSpec.groovy
+++ src/test/groovy/com/ruubel/massfollow/job/FollowStatsCleanupJobSpec.groovy
@@ -20,12 +20,12 @@ class FollowStatsCleanupJobSpec extends Specification {
     def "when cleanup job runs, then only selected ones are removed from DB" () {
         given:
             List<FollowingAmount> list = new ArrayList<FollowingAmount>(){{
-                new FollowingAmount(id: 1, created: Instant.now())
-                new FollowingAmount(id: 2, created: Instant.now().plusSeconds(60))
-                new FollowingAmount(id: 3, created: Instant.now().plusSeconds(70))
-                new FollowingAmount(id: 4, created: Instant.now().plus(1, ChronoUnit.DAYS))
-                new FollowingAmount(id: 5, created: Instant.now().plus(1, ChronoUnit.DAYS).plusSeconds(60))
-                new FollowingAmount(id: 6, created: Instant.now().plus(2, ChronoUnit.DAYS))
+                add(new FollowingAmount(id: 1, created: Instant.now()))
+                add(new FollowingAmount(id: 2, created: Instant.now().plusSeconds(60)))
+                add(new FollowingAmount(id: 3, created: Instant.now().plusSeconds(70)))
+                add(new FollowingAmount(id: 4, created: Instant.now().plus(1, ChronoUnit.DAYS)))
+                add(new FollowingAmount(id: 5, created: Instant.now().plus(1, ChronoUnit.DAYS).plusSeconds(60)))
+                add(new FollowingAmount(id: 6, created: Instant.now().plus(2, ChronoUnit.DAYS)))
             }}
         when:
             job.cleanup()
